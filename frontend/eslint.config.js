95d7f5ccaf7cfc7975030dd473f407e66a75d838
Jordi López <jordilg@msn.com>
2025-09-21 19:04:54 +0200

    Fix lint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -7,29 +7,33 @@ import prettierESLint from 'eslint-plugin-prettier/recommended';
 
 export default [
   prettierESLint,
-  ...tseslint.config(
-    {
-      ignores: ['dist', 'node_modules'],
+  {
+    ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
+  },
+  ...tseslint.config({
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        React: 'readonly',
+      },
+      parserOptions: {
+        project: ['./tsconfig.app.json', './tsconfig.configs.json', './tsconfig.node.json'],
+      },
     },
-    {
-      extends: [js.configs.recommended, ...tseslint.configs.recommended],
-      files: ['**/*.{ts,tsx}'],
-      languageOptions: {
-        ecmaVersion: 'latest',
-        globals: globals.browser,
-        parserOptions: {
-          project: ['./tsconfig.app.json', './tsconfig.configs.json'],
-        },
-      },
-      plugins: {
-        'react-hooks': reactHooks,
-        'react-refresh': reactRefresh,
-      },
-      rules: {
-        ...reactHooks.configs.recommended.rules,
-        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-        '@typescript-eslint/consistent-type-exports': 'error',
-      },
-    }
-  ),
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      '@typescript-eslint/consistent-type-exports': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  }),
 ];
