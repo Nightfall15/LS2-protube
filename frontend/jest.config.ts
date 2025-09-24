7c0c808f59147887a361dd8447ffc196d5efcc01
Jordi López <jordilg@msn.com>
2025-09-24 09:51:12 +0200

    Fix test

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -5,7 +5,7 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 /** @type {import("jest").Config} **/
 export default {
   preset: 'ts-jest',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
   transform: {
     ...tsJestTransformCfg,
